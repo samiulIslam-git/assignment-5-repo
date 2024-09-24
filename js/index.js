d20504993f6c3949958e5d07289c5ee74d2cf382
samiulIslam-git <kh.sami2017@gmail.com>
2024-09-25 04:03:22 +0600

    activated and calculated 3 cards with js

--- js/index.js
+++ js/index.js
@@ -12,8 +12,34 @@ donateButton.addEventListener("click", function(){
 
     const minus = document.getElementById("main-balance");
     minus.innerText = sumBalance;
+})
 
-    if (mainBalance < 0 ){
-        alert("you don/t have money");
-    }
+
+const btnFeni = document.getElementById("donate-btn-feni");
+btnFeni.addEventListener("click", function(){
+    const feniDonate = parseFloat(document.getElementById("feni-donate").value);
+    const addedBalanceFeni = parseFloat(document.getElementById("added-balance-feni").innerText);
+    const mainBalanceTwo = parseFloat(document.getElementById("main-balance").innerText);
+
+    const minusBalance = mainBalanceTwo - feniDonate;
+    const totalBalanceTwo = feniDonate + addedBalanceFeni;
+    const showTwo = document.getElementById("added-balance-feni");
+    showTwo.innerText = totalBalanceTwo;
+    const minusTwo = document.getElementById("main-balance");
+    minusTwo.innerText = minusBalance;
+})
+
+// 3rd card
+
+const btnQuata = document.getElementById("quota-btn");
+btnQuata.addEventListener("click", function(){
+    const quotaDonate = parseFloat(document.getElementById("quota-donate").value);
+    const addedBalanceQuota = parseFloat(document.getElementById("added-balance-quota").innerText);
+    const mainBalanceThree = parseFloat(document.getElementById("main-balance").innerText);
+    const minusBalanceTwo = mainBalanceThree - quotaDonate;
+    const totalBalanceThree = quotaDonate + addedBalanceQuota;
+    const showThree = document.getElementById("added-balance-quota");
+    showThree.innerText = totalBalanceThree;
+    const minusThree = document.getElementById("main-balance");
+    minusThree.innerText = minusBalanceTwo;
 })
