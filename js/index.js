f6095b9c8aafd3ccaf6b36fd8b240da68fb3f6fd
samiulIslam-git <kh.sami2017@gmail.com>
2024-09-25 15:08:08 +0600

    connected home and blog html file

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ donateButton.addEventListener("click", function(){
     const noakhaliDonate = parseFloat(document.getElementById("noakhali-donate").value);
     const mainBalance = parseFloat(document.getElementById("main-balance").innerText);
     const addedBalance = parseFloat(document.getElementById("added-balance").innerText);
-    
+
     const sumBalance = mainBalance - noakhaliDonate;
     const totalBalance = noakhaliDonate + addedBalance;
     
@@ -12,8 +12,13 @@ donateButton.addEventListener("click", function(){
 
     const minus = document.getElementById("main-balance");
     minus.innerText = sumBalance;
+
+    const donateButton = alert("congrats you donate some taka")
+    donateButton.innerText;
+
 })
 
+// 2nd card
 
 const btnFeni = document.getElementById("donate-btn-feni");
 btnFeni.addEventListener("click", function(){
@@ -27,6 +32,9 @@ btnFeni.addEventListener("click", function(){
     showTwo.innerText = totalBalanceTwo;
     const minusTwo = document.getElementById("main-balance");
     minusTwo.innerText = minusBalance;
+
+    const btnFeni = alert("congrats you donate some taka")
+    btnFeni.innerText;
 })
 
 // 3rd card
@@ -42,4 +50,28 @@ btnQuata.addEventListener("click", function(){
     showThree.innerText = totalBalanceThree;
     const minusThree = document.getElementById("main-balance");
     minusThree.innerText = minusBalanceTwo;
+
+    const btnQuata = alert("congrats you donate some taka")
+    btnQuata.innerText;
+})
+
+
+// history btn
+
+const historyTab = document.getElementById("history-tab");
+const donationTab = document.getElementById("donation-tab");
+historyTab.addEventListener("click", function(){
+    historyTab.classList.add('btn','btn-secondary','bg-green-400','font-bold');
+
+    donationTab.classList.remove('btn','btn-secondary','bg-green-400','font-bold');
+
+    document.getElementById("main").classList.add("hidden");
+})
+
+donationTab.addEventListener("click", function(){
+    donationTab.classList.add('btn','btn-secondary','bg-green-400','font-bold');
+
+    historyTab.classList.remove('btn','btn-secondary','bg-green-400','font-bold');
+
+    document.getElementById("main").classList.remove("hidden");
 })
